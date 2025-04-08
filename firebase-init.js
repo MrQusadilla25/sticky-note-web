52d10c76f3617daca40332d03db6e261dd974f19
MrQusadilla25 <ikertyph@gmail.com>
2025-04-08 14:48:59 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,10 +1,13 @@
-// Firebase config + initialization
+// firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDcnxp_iGUMTp4klEhiB5sCcCvh6IAxe9Y",
   authDomain: "stickynoteapp-883b8.firebaseapp.com",
+  databaseURL: "https://stickynoteapp-883b8-default-rtdb.firebaseio.com",
   projectId: "stickynoteapp-883b8",
   storageBucket: "stickynoteapp-883b8.appspot.com",
   messagingSenderId: "637398429722",
@@ -13,6 +16,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+getAnalytics(app);
 
-window.auth = auth;
+export const auth = getAuth(app);
+export const db = getDatabase(app);
