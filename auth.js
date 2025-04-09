e3cfca5e5659278d6ff74f3d85f1ca14cbfa4063
MrQusadilla25 <ikertyph@gmail.com>
2025-04-08 23:19:32 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,80 +1,19 @@
-// auth.js
-import { auth, db } from './firebase-init.js';
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import {
-  ref,
-  set,
-  update,
-  onDisconnect
-} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const authArea = document.getElementById("auth-area");
-const main = document.querySelector("main");
+const auth = window.auth;
 
-window.signup = function () {
-  const email = emailInput.value;
-  const password = passwordInput.value;
+window.login = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  signInWithEmailAndPassword(auth, email, password).catch(err => alert(err.message));
+};
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      const userRef = ref(db, 'users/' + user.uid);
-      set(userRef, {
-        displayName: "NO DISPLAY NAME",
-        status: "online"
-      });
-    })
-    .catch((error) => {
-      alert(error.message);
+window.signup = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  createUserWithEmailAndPassword(auth, email, password).then(userCred => {
+    updateProfile(userCred.user, {
+      displayName: email.split("@")[0]
     });
+  }).catch(err => alert(err.message));
 };
-
-window.login = function () {
-  const email = emailInput.value;
-  const password = passwordInput.value;
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      const userRef = ref(db, 'users/' + user.uid);
-
-      update(userRef, {
-        status: "online"
-      });
-
-      // Set offline status on disconnect
-      onDisconnect(ref(db, 'users/' + user.uid + '/status')).set("offline");
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-};
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    authArea.style.display = "none";
-    main.style.display = "block";
-  } else {
-    authArea.style.display = "block";
-    main.style.display = "none";
-  }
-});
-
-// Optional: If you want a sign out button somewhere
-window.logout = function () {
-  const user = auth.currentUser;
-  if (user) {
-    const userRef = ref(db, 'users/' + user.uid);
-    update(userRef, { status: "offline" });
-  }
-
-  signOut(auth);
-};
-
