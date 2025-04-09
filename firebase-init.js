28ce9acdb7a07caa8ccdfb7f6a48bc03996d51ec
MrQusadilla25 <ikertyph@gmail.com>
2025-04-09 11:48:40 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,23 +1,5 @@
-// Initialize Firebase (your own config here)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import { signOut } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
-const logoutBtn = document.getElementById("logoutBtn");
-
-logoutBtn?.addEventListener("click", () => {
-  signOut(auth)
-    .then(() => {
-      alert("You have been logged out.");
-      window.location.reload(); // or redirect to login page if you have one
-    })
-    .catch((error) => {
-      console.error("Logout error:", error);
-      alert("Error logging out.");
-    });
-});
-
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDcnxp_iGUMTp4klEhiB5sCcCvh6IAxe9Y",
   authDomain: "stickynoteapp-883b8.firebaseapp.com",
@@ -29,11 +11,4 @@ const firebaseConfig = {
   measurementId: "G-D27CEZ4WJN"
 };
 
-// Initialize the Firebase app with the config
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Auth
-const auth = getAuth(app);
-
-// Make the auth object globally available
-window.auth = auth;
