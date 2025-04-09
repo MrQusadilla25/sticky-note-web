e889cbd13d2bfde1613c9e7c2515ad7d0f76c40c
MrQusadilla25 <ikertyph@gmail.com>
2025-04-09 00:49:58 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDcnxp_iGUMTp4klEhiB5sCcCvh6IAxe9Y",
   authDomain: "stickynoteapp-883b8.firebaseapp.com",
@@ -13,6 +14,11 @@ const firebaseConfig = {
   measurementId: "G-D27CEZ4WJN"
 };
 
+// Initialize the Firebase app with the config
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth
 const auth = getAuth(app);
+
+// Make the auth object globally available
 window.auth = auth;
