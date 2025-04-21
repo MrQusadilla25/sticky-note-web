9f3a79a441ddf68510cc61700256bfd648be9d19
MrQusadilla25 <ikertyph@gmail.com>
2025-04-21 09:23:21 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,19 +1,24 @@
+// Firebase core imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyCu5Rjah9i0_Q7W8-auTgpy4X7THw3Y6Zw",
+  authDomain: "sendmeanote-99621.firebaseapp.com",
+  projectId: "sendmeanote-99621",
+  storageBucket: "sendmeanote-99621.firebasestorage.app",
+  messagingSenderId: "172688156945",
+  appId: "1:172688156945:web:e66ddb5f93bfd1cdf9c4b0",
+  measurementId: "G-L9WVZR1LDV"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-export { auth, db };
+export { app, analytics, auth, db };
