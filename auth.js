cd9b93df3fffeb43924f3923eee6d100c3b6d666
MrQusadilla25 <ikertyph@gmail.com>
2025-04-21 09:35:51 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,45 +1,25 @@
 import { auth, db } from './firebase-init.js';
 import {
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged
+  signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
-import { ref, set, get } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+import { ref, set } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 
-document.getElementById('sign-up').addEventListener('click', () => {
-  const email = email.value;
-  const password = password.value;
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(userCred => {
-      const uid = userCred.user.uid;
-      const device = navigator.userAgent;
-      set(ref(db, `users/${uid}`), {
-        email,
-        displayName: email.split('@')[0],
-        device
+// Sign up function
+export function signUpUser(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      const deviceName = navigator.userAgent;
+
+      return set(ref(db, `users/${user.uid}`), {
+        email: user.email,
+        displayName: "New User",
+        bio: "",
+        note: "",
+        color: "#ffff88",
+        device: deviceName,
+        suspended: false
       });
     });
-});
-
-document.getElementById('sign-in').addEventListener('click', () => {
-  const email = email.value;
-  const password = password.value;
-  signInWithEmailAndPassword(auth, email, password);
-});
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    const uid = user.uid;
-    const device = navigator.userAgent;
-    get(ref(db, `users/${uid}/suspended`)).then(snapshot => {
-      if (snapshot.exists() && snapshot.val() === true) {
-        alert("Account suspended: " + snapshot.val().reason);
-        auth.signOut();
-      } else {
-        document.getElementById('auth-section').classList.add('hidden');
-        document.getElementById('main-app').classList.remove('hidden');
-        document.getElementById('displayName').innerText = user.email.split('@')[0];
-      }
-    });
-  }
-});
+}
