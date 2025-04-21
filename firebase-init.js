e2485ee221c6750e549ff7f3962188abb00081d9
MrQusadilla25 <ikertyph@gmail.com>
2025-04-21 10:02:45 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,21 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCu5Rjah9i0_Q7W8-auTgpy4X7THw3Y6Zw",
   authDomain: "sendmeanote-99621.firebaseapp.com",
+  databaseURL: "https://sendmeanote-99621-default-rtdb.firebaseio.com",
   projectId: "sendmeanote-99621",
-  storageBucket: "sendmeanote-99621.firebasestorage.app",
+  storageBucket: "sendmeanote-99621.appspot.com",
   messagingSenderId: "172688156945",
   appId: "1:172688156945:web:e66ddb5f93bfd1cdf9c4b0",
   measurementId: "G-L9WVZR1LDV"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getDatabase(app);
-
-export { app, analytics, auth, db };
+export const auth = getAuth(app);
+export const db = getDatabase(app);
