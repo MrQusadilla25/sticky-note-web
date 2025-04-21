18f79aca7a9ea4d7923e9d352fc593dd57c3a099
MrQusadilla25 <ikertyph@gmail.com>
2025-04-21 13:30:35 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,20 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, update, child, get } from "firebase/database";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCu5Rjah9i0_Q7W8-auTgpy4X7THw3Y6Zw",
-    authDomain: "sendmeanote-99621.firebaseapp.com",
-    projectId: "sendmeanote-99621",
-    storageBucket: "sendmeanote-99621.firebasestorage.app",
-    messagingSenderId: "172688156945",
-    appId: "1:172688156945:web:e66ddb5f93bfd1cdf9c4b0",
-    measurementId: "G-L9WVZR1LDV"
+  apiKey: "AIzaSyCu5Rjah9i0_Q7W8-auTgpy4X7THw3Y6Zw",
+  authDomain: "sendmeanote-99621.firebaseapp.com",
+  projectId: "sendmeanote-99621",
+  storageBucket: "sendmeanote-99621.firebasestorage.app",
+  messagingSenderId: "172688156945",
+  appId: "1:172688156945:web:e66ddb5f93bfd1cdf9c4b0",
+  measurementId: "G-L9WVZR1LDV"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
+
+export { db, auth };
